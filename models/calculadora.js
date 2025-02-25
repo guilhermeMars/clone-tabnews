adcae8f59e630c8927efaa2d045f93a588776277
Guilherme Spiandorin <guilherme.spiandorin@bankeiro.com.br>
2025-02-25 14:55:18 -0300

    feat: add  endpoint

--- models/calculadora.js
+++
@@ -1,9 +0,0 @@
-function somar(num1, num2) {
-  const result = num1 + num2;
-  if (typeof result !== "number") {
-    return "Erro";
-  }
-  return result;
-}
-
-exports.somar = somar;
---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,5 @@
+function status(req, res) {
+  res.status(200).send("Porra d faculdade");
+}
+
+export default status;
